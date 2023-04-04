fa7f4b1bea35a68b302dae0a5144ddc4ba80a6c7
Gustavo Moura Barros <gustavomourago@gmail.com>
2023-04-03 22:14:31 -0300

    chapter 2 50%

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import theme from '@theme/index';
+
+import { Loading } from '@components/Loading';
 import { Groups } from '@screens/Groups';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle='light-content' backgroundColor='transparent' translucent />
+      { fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   );
 }
